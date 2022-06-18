b88c00cb9527748793d5a3fb76765ec20879b42d
Mederis Frambourt <mframbou@student.42nice.fr>
2022-06-18 14:53:46 +0200

    Now working on every machine and not only localhost

--- cgi-bin/cookie_creator.py
+++ cgi-bin/cookie_creator.py
@@ -11,7 +11,7 @@ username = form.getvalue('username')
 pouet = [
 	f"Set-Cookie:username = {username}",
 	"Max-Age = 3600",
-	"Domain = localhost",
+	#"Domain = localhost", # No domain so it works on all machines (if no domain is provided, the value will be the host of the url of current document)
 	"Path = /",
 ]
 
